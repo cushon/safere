9263e20fd5ce5c2508a3f95a5a2c8c3a44184b4d
Liam Miller-Cushon <cushon@google.com>
2026-08-10 14:11:21 +0200

    Disable RejectPrefilterTest for crosscheck

--- safere/src/test/java/org/safere/RejectPrefilterTest.java
+++ safere/src/test/java/org/safere/RejectPrefilterTest.java
@@ -11,6 +11,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 import org.safere.Pattern.CharClassScanInfo;
 
+@DisabledForCrosscheck("implementation test uses package-private SafeRE internals")
 class RejectPrefilterTest {
 
   @Test
