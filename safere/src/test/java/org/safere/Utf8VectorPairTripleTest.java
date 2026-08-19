2fdd1bf090d437737fc0f883179947e011150e10
Liam Miller-Cushon <cushon@google.com>
2026-08-19 13:01:34 +0200

    Disable internal Utf8VectorPairTripleTest for crosscheck

--- safere/src/test/java/org/safere/Utf8VectorPairTripleTest.java
+++ safere/src/test/java/org/safere/Utf8VectorPairTripleTest.java
@@ -14,6 +14,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+@DisabledForCrosscheck("implementation test uses package-private SafeRE internals")
 class Utf8VectorPairTripleTest {
 
   private static boolean isVectorApiAvailable() {
