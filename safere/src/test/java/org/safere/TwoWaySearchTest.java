964f55628df9abe0ddeeefc3a8332a9ea19d8891
Liam Miller-Cushon <cushon@google.com>
2026-08-27 00:14:46 +0200

    Exclude TwoWaySearchTest from crosscheck

--- safere/src/test/java/org/safere/TwoWaySearchTest.java
+++ safere/src/test/java/org/safere/TwoWaySearchTest.java
@@ -15,6 +15,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+@DisabledForCrosscheck("Package-private SafeRE internals")
 final class TwoWaySearchTest {
 
   @Test
